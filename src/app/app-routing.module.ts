5345c0db6c82efec031d7fec4aa917a35cea7fa4
samsung <samsung@DESKTOP-LGSDQV0>
2024-01-17 22:00:40 +0500

    front tayyor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WorksComponent } from './pages/works/works.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ReviewsComponent } from './pages/reviews/reviews.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+ {path:"works",component:WorksComponent},
+ {path:"blog",component:BlogComponent},
+ {path:"services",component:ServicesComponent},
+ {path:"about",component:AboutComponent},
+ {path:"reviews",component:ReviewsComponent},
+ {path:'contact',component:ContactComponent},
+ {path:"**",component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { WorksComponent } from './pages/works/works.component';
+import { ReviewsComponent } from './pages/reviews/reviews.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactComponent,
+    ServicesComponent,
+    WorksComponent,
+    ReviewsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
